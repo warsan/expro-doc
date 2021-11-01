bd6c2884152bb2dabcb9d64bc09ac0917b15c723
Stackbit Code Editor <projects@stackbit.com>
2021-11-01 13:37:21 +0000

    docs.js: changed

--- src/layouts/docs.js
+++ src/layouts/docs.js
@@ -158,7 +158,7 @@ export default class Docs extends React.Component {
                         </article>
                         <nav id="page-nav" className="page-nav">
                             <div id="page-nav-inside" className="page-nav-inside sticky">
-                                <h2 className="page-nav-title">Jump to Section</h2>
+                                <h2 className="page-nav-title">Перейти к разделу</h2>
                                 {headerLinks}
                             </div>
                         </nav>
