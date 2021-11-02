7af77c4f2eef42106deb54cd95f2e2638997acaa
Stackbit Code Editor <projects@stackbit.com>
2021-11-02 17:33:15 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -13,8 +13,8 @@ module.exports = {
         // Скажите webpack игнорировать просмотр файлов содержимого в папке content.
         // В противном случае webpack перекомпилирует приложение и обновит всю страницу.
         // Вместо этого в src/pages/[...slug].js используется функция "withRemoteDataUpdates"
-        // function to update the content on the page without refreshing the
-        // whole page
+        // function to update the content on the page without refreshing the whole page
+        //
         config.plugins.push(new webpack.WatchIgnorePlugin([[/\/content\//]]));
         return config;
     }
