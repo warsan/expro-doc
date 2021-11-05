02acda1ec585dc9220dbc93f701c04a26f2f55a9
Stackbit Code Editor <projects@stackbit.com>
2021-11-05 11:55:22 +0000

    getPages.js: changed

--- src/utils/getPages.js
+++ src/utils/getPages.js
@@ -20,7 +20,7 @@ import _ from 'lodash';
  * ]
  *
  *
- * @param {Array} pages Array of page objects. All pages must have '__metadata.urlPath' field.
+ * @param {Array} pages Массив объектов страниц. Все страницы должны иметь поле '__metadata.urlPath'.
  * @param {string} urlPath The url path to filter pages by
  * @return {Array}
  */
